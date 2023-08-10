96b83fcc7ee56df504e9cb0a2ee5372868dd90f2
Dmitry <dima.captan@yandex.ru>
2023-08-10 12:48:14 +0300

    added a command to fill bd with data from JSON-file

---
+++ catalog/management/commands/fill.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+from django.core.management import BaseCommand
+from catalog.models import Category, Product
+
+JSON_FILE = os.path.abspath('database_data.json')
+
+
+class Command(BaseCommand):
+    '''
+    Команда для заполнения данных в базу данных
+    с предварительной отчисткой данных
+    '''
+
+    def handle(self, *args, **options) -> None:
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+
+        category_objects = []
+        product_objects = []
+
+        with open(JSON_FILE, 'r', encoding='UTF-8') as j_file:
+            models_data = json.load(j_file)
+
+        for data in models_data:
+            if data['model'] == 'catalog.category':
+                category_objects.append(Category(pk=data['pk'], **data['fields']))
+        Category.objects.bulk_create(category_objects)
+
+        for data in models_data:
+            if data['model'] == 'catalog.product':
+                category_id = data['fields'].pop('category')
+                category = Category.objects.get(pk=category_id)
+                product_objects.append(Product(pk=data['pk'], category=category, **data['fields']))
+        Product.objects.bulk_create(product_objects)
