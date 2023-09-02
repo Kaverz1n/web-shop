8f304d4e4570ff28931764ff9e156918dc7464ea
Dmitry <dima.captan@yandex.ru>
2023-09-02 15:54:31 +0300

    added commas in tuples

--- blog/admin.py
+++ blog/admin.py
@@ -3,10 +3,9 @@ from django.contrib import admin
 from blog.models import Article
 
 
-# Register your models here.
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'created_at', 'is_published', 'views_counter')
+    list_display = ('pk', 'title', 'created_at', 'is_published', 'views_counter',)
     list_filter = ('is_published',)
-    search_fields = ('title', 'body')
+    search_fields = ('title', 'body',)
     ordering = ('pk',)
