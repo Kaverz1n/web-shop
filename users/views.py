64a61fe0cfd03edd38df3992ec2c9ffca6b2d1e9
Dmitry <dima.captan@yandex.ru>
2023-09-02 16:09:19 +0300

    added description in views and registered user belongs to sellers after registration

--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 import random
+from typing import Any
 
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Group
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.utils.encoding import force_bytes
@@ -12,6 +14,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.views import View
 from django.views.generic import CreateView, TemplateView, UpdateView
 
+from catalog.services import send_email
 from shop import settings
 
 from users.forms import RegisterForm, ResetPasswordForm, ProfileUpdateForm
@@ -19,12 +22,16 @@ from users.models import User
 
 
 class RegisterView(CreateView):
+    '''
+    Класс для регистрации пользователей
+    Класс для регистрации пользователей
+    '''
     model = User
     form_class = RegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
 
-    def form_valid(self, form):
+    def form_valid(self, form) -> HttpResponse:
         if form.is_valid():
             self.object = form.save()
             self.object.is_active = False
@@ -44,14 +51,18 @@ class RegisterView(CreateView):
 
             return redirect('users:sent_email')
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['title'] = 'Регистрация'
         return context
 
 
 class UserConfirmEmailView(View):
-    def get(self, request, uidb64, token):
+    '''
+    Класс для проверки подтверждения пользователя по email
+    '''
+
+    def get(self, request, uidb64, token) -> HttpResponse:
         try:
             uid = urlsafe_base64_decode(uidb64)
             user = User.objects.get(pk=uid)
@@ -59,9 +70,8 @@ class UserConfirmEmailView(View):
             user = None
 
         if user is not None and default_token_generator.check_token(user, token):
-            permission1 = Permission.objects.get(codename='add_product', content_type__app_label='catalog')
-            permission2 = Permission.objects.get(codename='check_products', content_type__app_label='catalog')
-            user.user_permissions.add(permission1, permission2)
+            group = Group.objects.get(name='seller')
+            user.groups.add(group)
             user.is_active = True
             user.save()
             login(request, user)
@@ -71,38 +81,66 @@ class UserConfirmEmailView(View):
 
 
 class EmailConfirmSentView(TemplateView):
+    '''
+    Класс для отображения страницы с оповещением об отправленом
+    письме
+    '''
     template_name = 'users/sent_email.html'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['title'] = 'Подтверждение email'
 
+        return context
+
 
 class EmailConfirmedView(TemplateView):
+    '''
+    Класс для отображения страницы с оповещением об
+    подтвержденом e-mail адресе
+    '''
     template_name = 'users/email_confirmed.html'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['title'] = 'Email подтвержден'
 
+        return context
+
 
 class EmailFailedConfirmView(TemplateView):
+    '''
+    Класс для отображения страницы с оповещением об
+    неподтвержденом e-mail адресе
+    '''
     template_name = 'users/failed_email_confirm.html'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['title'] = 'Ошибка подтверждения email'
 
+        return context
+
 
 class PasswordResetSentView(TemplateView):
+    '''
+    Класс для отображения страницы с оповещением об успешной
+    отправки нового пароля на почту
+    '''
     template_name = 'users/sent_password.html'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['title'] = 'Пароль отправлен'
 
+        return context
 
-def get_new_password(request):
+
+def get_new_password(request) -> HttpResponse:
+    '''
+    Генерация нового пароля с последующей отправкой на почту
+    пользователя
+    '''
     form = ResetPasswordForm()
     if request.method == 'POST':
         form = ResetPasswordForm(request.POST)
@@ -111,11 +149,11 @@ def get_new_password(request):
             password = ''.join([str(random.randint(0, 10)) for _ in range(10)])
             user.set_password(password)
             user.save()
-            send_mail(
+
+            send_email(
+                user.email,
                 'Новый пароль!',
                 f'Сгенирированный пароль для входа в систему: {password}',
-                settings.EMAIL_HOST_USER,
-                [user.email]
             )
             return redirect('users:sent_password')
 
@@ -123,15 +161,18 @@ def get_new_password(request):
 
 
 class UserProfileUpdateView(LoginRequiredMixin, UpdateView):
+    '''
+    Класс для редактирования профиля пользователя
+    '''
     model = User
     form_class = ProfileUpdateForm
     template_name = 'users/profile.html'
     success_url = reverse_lazy('users:profile')
 
-    def get_object(self, queryset=None):
+    def get_object(self, queryset=None) -> User:
         return self.request.user
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['title'] = f'Профиль {self.request.user.email}'
         return context
